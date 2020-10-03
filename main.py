25256722379a5e350f47984f1325f924a8153f12
unknown <your-email>
2020-10-03 23:19:39 +0530

    answer_12

--- main.py
+++ main.py
@@ -6,21 +6,35 @@ Answer the following questions.
 """
 
 # 1.- Make a program that solves and shows the summation of 64 + 32
+from os import name
+
+
 print(64+32)
 #test
 
 # 2.- Do the same as the question one but this time use variables instead of 
-a=64+32
-print(a)
+x = 64
+y = 32
+sum = x + y
+print(sum)
 # numbers.
 
 # 3.- Make a program that prints a sentence that includes at least 3 variables.
-
+x = hello
+y = name
+z = Bunny
+print(x + "my" + y + "is" +z)
 
 # 4.- Given a sentence, assign the string to a variable then print the number of 
+str = "This is my first python program"
+print len(str)
+
 # characters in the sentence. 
 # The sentence is "This is my first Python program."
 
 # 5.- Given the resolution 1920 x 1080, make a program that prints a string with 
+x = (1920 * 10) / 100
+y = (1080 * 10) / 100
+print(" The 10% overscan of 1920 is "+ x +", and the 1080 is " + y".")
 # the 10% over-scan value of those numbers. The printed string must be as 
 # follows: "The 10% overscan of 1920 is <value 1>, and the 1080 is <value 2>"
